a78b6330d382f50999a5107e0714fe1097e55745
Jihyun Nam <initialfx@gmail.com>
2022-11-18 21:06:11 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+
+	def function(self):
+		pass
+		
+def function():
+	pass
